2046ee48d0dcb8d6f86189c6e05c57525dda094c
beefycode <koru.louis@gmail.com>
2023-01-13 13:15:41 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='Heading'>Hello Dojo!</h1>
+      <h2 className='SubTitle'>Things I need to do:</h2>
+      <ul className='List'>
+        <li>Learn React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
